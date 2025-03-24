3e1cff25de02436f1f0caeae27e824f424482413
danielemurgioni <dashomew@gmail.com>
2025-03-24 16:24:31 +0100

    [FEAT] Created function exercise-4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -4,7 +4,15 @@ const word = 'javascript';
 
 
 // Dichiara la funzione qui.
-
+function numberOfVocalsInWord(string){
+    let count = 0;
+    for(let i=0; i<string.length; i++){
+        if(string[i] === "a" || string[i] === "e" || string[i] === "i" || string[i] === "o" || string[i] === "u"){
+            count++;
+        }
+    }
+    return count;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
